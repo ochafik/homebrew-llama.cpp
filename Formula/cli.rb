42fdc5903e82d84a97c39387d74dd132f549e0cb
Olivier Chafik <ochafik@google.com>
2024-05-14 14:07:02 +0100

    Update cli.rb

--- Formula/cli.rb
+++ Formula/cli.rb
@@ -1,15 +1,19 @@
 class Cli < Formula
   desc "Port of Facebook's LLaMA model in C/C++"
   homepage "https://github.com/ggerganov/llama.cpp"
-  url "https://github.com/ochafik/llama.cpp/archive/22040277f15efe4e77a428bf5724ffdbe2a1a888/cli-tmp.tar.gz"
-  version "22040277f15efe4e77a428bf5724ffdbe2a1a888"
-  sha256 "bb9ed66a9553b4ac4e86706faf36e707b1f1ef19c9c2a845137ee78963a89e0c"
+
+  url "https://github.com/ochafik/llama.cpp.git",
+    revision: "663300ca2933cf6513e93b74c8cfe66984247512"
+
+  head "https://github.com/ochafik/llama.cpp.git",
+    branch: "cli"
 
   license "MIT"
 
   depends_on "cmake" => :build
   depends_on "curl" => :build
   depends_on "openblas" => :build
+  depends_on :macos
 
   def install
     system "cmake", "-B", "build",
